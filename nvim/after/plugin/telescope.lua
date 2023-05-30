d7c2e52899562582d0604fd275c417ae25b0ed39
softMachina <kai.k770@live.de>
2023-05-30 20:00:57 +0200

    change conf

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,9 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps',function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+vim.keymap.set('n', '<leader>ps',function()
+	builtin.grep_string({ search = vim.fn.input("grep > ") });
+end)
+vim.keymap.set('n', '<leader>pt',function()
+builtin.grep_string({ search = vim.fn.input(":Telescope live_grep")});
 end)
--- nvim/lua/lua_conf/remap.lua
+++ nvim/lua/lua_conf/remap.lua
@@ -5,5 +5,3 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- replace the current word you're at.
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
